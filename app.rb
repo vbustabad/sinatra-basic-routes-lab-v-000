d85efe408ecc45deedee6a4c27f94cf57a7cb4c7
Veronica Bustabad <vbustabad@gmail.com>
2018-07-05 17:32:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Veronica Bustabad."
+  end
+
+  get '/hometown' do
+    "My hometown is Miami, Florida."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Four Seasons by Antonio Vivaldi."
+  end
+
 end
